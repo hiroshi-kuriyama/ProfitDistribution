5a1c05149d1e4fe5f836009b1eb499d91394f863
hiroshi-kuriyama <29877317+hiroshi-kuriyama@users.noreply.github.com>
2023-07-31 20:21:18 +0900

    Update ProfitDistribution.sol

--- contracts/ProfitDistribution.sol
+++ contracts/ProfitDistribution.sol
@@ -7,31 +7,51 @@ contract ForwardEther {
     }
     Share[] public shares;
     address payable treasuryAddress;
-    // address payable[] public destinationAddresses;
-    // uint256[] public percentages;
 
     constructor(address payable _treasuryAddress, address payable[] memory _destinationAddresses, uint256[] memory _percentages) {
+        // アドレスと分配比率の配列のlengthが異なる場合のエラー処理
+        require(_destinationAddresses.length == _percentages.length, "Invalid input. Length of _destinationAddresses and _percentages are not same.");
+        
         treasuryAddress = _treasuryAddress;
-        // uint256 totalPercentage = 0;
 
-        // for (uint256 i = 0; i < _percentages.length; i++) {
-        //     totalPercentage += _percentages[i];
-        // }
         for (uint256 i = 0; i < _destinationAddresses.length; i++) {
             shares.push(Share(_destinationAddresses[i], _percentages[i]));
         }
-        // destinationAddresses = _destinationAddresses;
-        // percentages = _percentages;
+
     }
 
     // コントラクトに送金されたとき、その送金を自動的にdestinationAddressに転送します。
     receive() external payable {
         require(msg.value > 0, "No ethers transferred");
+
+        // トレジャリーには必ず30％を送金
         (bool success, ) = treasuryAddress.call{value: msg.value * 30 / 100}("");
         require(success, "Transfer failed");
+
+        // 残りの70%を分配比率に応じて送金（_percentagesの合計が70でない場合は正規化）
+        uint256 totalPercentage = 0;    // 分配比率の正規化のため
         for (uint256 i = 0; i < shares.length; i++) {
-            (bool success_i, ) = shares[i].destinationAddress.call{value: msg.value * shares[i].percentage / 100}("");
-            require(success_i, "Transfer failed in roop");
+            totalPercentage += shares[i].percentage;
         }
+        for (uint256 i = 0; i < shares.length; i++) {
+            (bool success_i, ) = shares[i].destinationAddress.call{value: msg.value * shares[i].percentage * 70 / totalPercentage / 100}("");
+            require(success_i, "Transfer failed in loop");
+        }
+    }
+
+    function getTreasuryAddress() public view returns (address) {
+        return treasuryAddress;
+    }
+
+    function getShares() public view returns (address[] memory, uint256[] memory) { // 構造体はそのままreturnできない。
+        address[] memory destinationAddresses = new address[](shares.length);
+        uint256[] memory percentages = new uint256[](shares.length);
+
+        for (uint i = 0; i < shares.length; i++) {
+            destinationAddresses[i] = shares[i].destinationAddress;
+            percentages[i] = shares[i].percentage;
+        }
+
+        return (destinationAddresses, percentages);
     }
 }
