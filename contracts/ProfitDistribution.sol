2ee7901991b7613efc56b0fc6ea164d771a082c0
hiroshi-kuriyama <29877317+hiroshi-kuriyama@users.noreply.github.com>
2023-08-03 12:50:59 +0900

    Add: TokenDistributor contract

--- contracts/ProfitDistribution.sol
+++ contracts/ProfitDistribution.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.6;
 
-contract ProfitDistribution {
+// Importing OpenZeppelin's ERC20 interface version 3.4.2
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.2/contracts/token/ERC20/IERC20.sol";
+
+contract ProfitDistributor {
 
     // 宛先アドレスと分配比率
     struct Share {
@@ -59,17 +62,87 @@ contract ProfitDistribution {
     }
 }
 
-contract DeployProfitDistribution {
-    address[] public deployedContracts; // デプロイしたコントラクトアドレスを（過去分を含めてすべて）記録するための配列
-    event ContractDeployed(address contractAddress); // Add event for logging
-    function deployProfitDistribution(address payable _treasuryAddress, address payable[] memory _destinationAddresses, uint256[] memory _percentages) public returns (ProfitDistribution) {
-        ProfitDistribution pd = new ProfitDistribution(_treasuryAddress, _destinationAddresses, _percentages);
-        address newContractAddress = address(pd);
-        deployedContracts.push(newContractAddress);
-        emit ContractDeployed(newContractAddress); // Emit event with new contract address
-        return pd;
+contract TokenDistributor {
+
+    struct Share {
+        address payable destinationAddress;
+        uint256 percentage;
     }
-    function getDeployedContracts() public view returns (address[] memory) {
-        return deployedContracts;
+
+    Share[] public shares;
+    IERC20 public token;
+
+    constructor(address payable[] memory _destinationAddresses, uint256[] memory _percentages, IERC20 _token) {
+        require(_destinationAddresses.length == _percentages.length, "Invalid input. Length of _destinationAddresses and _percentages are not same.");
+
+        token = _token;
+
+        for (uint256 i = 0; i < _destinationAddresses.length; i++) {
+            shares.push(Share(_destinationAddresses[i], _percentages[i]));
+        }
+    }
+
+    function distributeTokens(uint256 amount) public {
+        require(amount > 0, "No tokens to distribute");
+
+        uint256 totalPercentage = 0;
+        for (uint256 i = 0; i < shares.length; i++) {
+            totalPercentage += shares[i].percentage;
+        }
+        for (uint256 i = 0; i < shares.length; i++) {
+            require(token.transferFrom(msg.sender, shares[i].destinationAddress, amount * shares[i].percentage / totalPercentage), "Transfer in loop failed");
+        }
+    }
+
+    function getShares() public view returns (address[] memory, uint256[] memory) {
+        address[] memory destinationAddresses = new address[](shares.length);
+        uint256[] memory percentages = new uint256[](shares.length);
+
+        for (uint i = 0; i < shares.length; i++) {
+            destinationAddresses[i] = shares[i].destinationAddress;
+            percentages[i] = shares[i].percentage;
+        }
+
+        return (destinationAddresses, percentages);
+    }
+}
+
+
+
+
+contract ContractDeployer {
+    struct DeployedContract {
+        address profitDistributor;
+        address tokenDistributor;
+    }
+
+    DeployedContract[] public deployedContracts;
+    address payable treasuryAddress;
+    IERC20 public token;
+
+    event ContractDeployed(address profitDistributor, address tokenDistributor); 
+
+    constructor(address payable _treasuryAddress, IERC20 _token) {
+        treasuryAddress = _treasuryAddress;
+        token = _token;
+    }
+
+    function deployDistributors(address payable[] memory _destinationAddresses, uint256[] memory _percentages) public {
+        ProfitDistributor pd = new ProfitDistributor(treasuryAddress, _destinationAddresses, _percentages);
+        TokenDistributor td = new TokenDistributor(_destinationAddresses, _percentages, token);
+        deployedContracts.push(DeployedContract(address(pd), address(td)));
+        emit ContractDeployed(address(pd), address(td));
+    }
+
+    function getDeployedContracts() public view returns (address[] memory, address[] memory) {
+        address[] memory profitDistributors = new address[](deployedContracts.length);
+        address[] memory tokenDistributors = new address[](deployedContracts.length);
+
+        for (uint i = 0; i < deployedContracts.length; i++) {
+            profitDistributors[i] = deployedContracts[i].profitDistributor;
+            tokenDistributors[i] = deployedContracts[i].tokenDistributor;
+        }
+
+        return (profitDistributors, tokenDistributors);
     }
 }
